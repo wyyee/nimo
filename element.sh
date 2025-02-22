989b1974430f8426e837bae090e68efc50ebe6fd
wyyee wye <imhshuvo@gmail.com>
2025-02-22 20:36:19 +0000

    feat: add if to check file run argument

--- element.sh
+++ element.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+# Check if an argument is provided
+if [[ -z $1 ]]; then
+  echo "Please provide an element as an argument."
+  exit 0
+fi
