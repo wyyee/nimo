456c75ad80e1bc8f4d6c92f3fff951bc75755339
wyyee wye <imhshuvo@gmail.com>
2025-02-22 20:37:04 +0000

    feat: get element from sql

--- element.sh
+++ element.sh
@@ -7,3 +7,13 @@ if [[ -z $1 ]]; then
   echo "Please provide an element as an argument."
   exit 0
 fi
+
+ELEMENT=$($PSQL "
+  SELECT elements.atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius
+  FROM elements
+  JOIN properties ON elements.atomic_number = properties.atomic_number
+  JOIN types ON properties.type_id = types.type_id
+  WHERE elements.atomic_number::text = '$1'
+  OR symbol = '$1'
+  OR name = '$1'
+")
